1086bb17af50272c58af2f2098d7b2ab4973ec87
Lianna <liannanovitz@gmail.com>
2024-06-03 21:45:48 -0700

    add bare bones compare component

--- src/app/page.js
+++ src/app/page.js
@@ -5,28 +5,57 @@ import { useState } from 'react';
 
 export default function BrainDump() {
   const [taskList, setTaskList] = useState([]);
+  const [isPrioritizing, setIsPrioritizing] = useState(false);
   const [text, setText] = useState('');
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [compareToIndex, setCompareToIndex] = useState(0);
+  const [matches, setMatches] = useState(new Map)
 
-  return (
+  const currentTask = taskList[currentIndex];
+  const compareToTask = taskList[currentIndex+1];
+  console.log(currentTask);
+  console.log(compareToTask);
+
+  console.log("length ", taskList.length)
+
+  function createTasks() {
+    const initialTasks = [];
+    for (let i = 0; i < taskList.length; i++) {
+      initialTasks.push(taskList[i]);
+    }
+    console.log({initialTasks});
+    localStorage.setItem("tasks", initialTasks);
+    setIsPrioritizing(true);
+  }
+
+  const addTaskComponent = (
     <>
+      <button onClick={createTasks}>Done, let's prioritize</button>
       <input
         value={text}
         onChange={e => setText(e.target.value)}
       />
       <button onClick={() => {
+        taskList.push(text)
         setText('');
-        setTaskList([{
-          id: taskList.length,
-          text: text
-        }, ...taskList]);
+        setTaskList([...taskList]);
       }}>Add</button>
       <ul>
-        {taskList.map(item => (
-          <li key={item.id}>
-            {item.text}
+        {taskList.map((item, idx) => (
+          <li key={idx}>
+            {item}
           </li>
         ))}
       </ul>
     </>
   );
+
+  const compareComponent = (
+    <>
+    <div>{currentTask}</div>
+    <div>{compareToTask}</div>
+    </>
+  );
+
+  return isPrioritizing ? compareComponent : addTaskComponent;
 }
