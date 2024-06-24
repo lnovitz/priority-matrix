10d7c1b467e5f2ee2b0beba6a7eb0671dbe64f39
Lianna <liannanovitz@gmail.com>
2024-06-24 11:14:35 -0700

    fix tooltip

--- src/app/page.js
+++ src/app/page.js
@@ -312,7 +312,7 @@ export default function BrainDump() {
                 Continue
               </button>
               <span class="pointer-events-none absolute -top-7 left-0 w-max opacity-0 transition-opacity group-hover:opacity-100">
-                This is a button.
+                Click here once you are done adding tasks to the list.
               </span>
             </div>
           </div>
